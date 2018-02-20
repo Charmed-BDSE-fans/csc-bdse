cc49d19d83033ee24d5aef2fb9869c75cb596c62
Gleb Valin <the7winds@yandex.ru>
2018-02-20 21:19:02 +0300

    fix test initial part

--- bdse-integration-tests/src/test/java/ru/csc/bdse/kv/KeyValueApiHttpClientTest2.java
+++ bdse-integration-tests/src/test/java/ru/csc/bdse/kv/KeyValueApiHttpClientTest2.java
@@ -2,15 +2,20 @@ package ru.csc.bdse.kv;
 
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.images.RemoteDockerImage;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import ru.csc.bdse.util.Env;
 import ru.csc.bdse.util.Random;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.junit.Assert.*;
 
 /**
@@ -21,21 +26,34 @@ import static org.junit.Assert.*;
 public class KeyValueApiHttpClientTest2 {
 
     @ClassRule
-    public static final DockerComposeContainer composition =
-            new DockerComposeContainer(new File("src/test/resources/kvnode/docker-compose.yml"))
-                    .withEnv(Env.KVNODE_NAME, "node-0")
-                    .withExposedService("kvnode", 8080);
+    public static final GenericContainer db =
+            new GenericContainer(new RemoteDockerImage("postgres:10"))
+                    .withNetwork(Network.SHARED);
+
+    @ClassRule
+    public static final GenericContainer kvnode = new GenericContainer(
+            new ImageFromDockerfile()
+                    .withFileFromFile("target/bdse-kvnode-0.0.1-SNAPSHOT.jar", new File
+                            ("../bdse-kvnode/target/bdse-kvnode-0.0.1-SNAPSHOT.jar"))
+                    .withFileFromClasspath("Dockerfile", "kvnode/Dockerfile"))
+            .withNetwork(Network.SHARED)
+            .withEnv(Env.KVNODE_NAME, "node-0")
+            .withExposedPorts(8080)
+            .withStartupTimeout(Duration.of(30, SECONDS));
+
+
 
     private KeyValueApi api = newKeyValueApi();
 
     private KeyValueApi newKeyValueApi() {
-        final String baseUrl = "http://localhost:" + composition.getServicePort("kvnode", 8080);
+        final String baseUrl = "http://localhost:" + kvnode.getMappedPort(8080);
         return new KeyValueApiHttpClient(baseUrl);
     }
 
     @Test
     public void concurrentPuts() {
         // simultanious puts for the same key value
+
         api.action(Env.KVNODE_NAME, NodeAction.UP);
 
         final String key = Random.nextKey();
@@ -84,7 +102,10 @@ public class KeyValueApiHttpClientTest2 {
     @Test
     public void concurrentDeleteAndKeys() {
         // simultanious delete by key and keys listing
+
         final int ELEMENTS_NUM = 100000;
+
+        api.action(Env.KVNODE_NAME, NodeAction.UP);
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
