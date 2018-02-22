9e788cde74dab89be10387a26ca126c39b57849a
Gleb Valin <the7winds@yandex.ru>
2018-02-22 13:51:13 +0300

    move file

--- bdse-integration-tests/src/test/java/ru/csc/bdse/util/containers/Postgres.java
+++ bdse-integration-tests/src/test/java/ru/csc/bdse/util/containers/Postgres.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.time.Duration;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
-import static ru.csc.bdse.kv.KeyValueApiHttpClientTest2.testNetwork;
 
 public class Containers {
 
@@ -16,18 +15,18 @@ public class Containers {
                 new ImageFromDockerfile()
                         .withFileFromClasspath("kvdb.sql", "db/kvdb.sql")
                         .withFileFromClasspath("Dockerfile","db/Dockerfile")
-        ).withNetwork(testNetwork)
-                .withExposedPorts(5432);
+        ).withExposedPorts(5432)
+                .withStartupTimeout(Duration.of(30, SECONDS));
     }
 
-    public static GenericContainer kvnode() {
+    public static GenericContainer postgreskvnode() {
         return new GenericContainer(
                 new ImageFromDockerfile()
                         .withFileFromFile("target/bdse-kvnode-0.0.1-SNAPSHOT.jar", new File
                                 ("../bdse-kvnode/target/bdse-kvnode-0.0.1-SNAPSHOT.jar"))
                         .withFileFromClasspath("Dockerfile", "kvnode/Dockerfile"))
-                .withNetwork(testNetwork)
                 .withEnv(Env.KVNODE_NAME, "node-0")
+                .withEnv(Env.SPRING_PROFILES_ACTIVE, "postgres")
                 .withExposedPorts(8080)
                 .withStartupTimeout(Duration.of(30, SECONDS));
     }
