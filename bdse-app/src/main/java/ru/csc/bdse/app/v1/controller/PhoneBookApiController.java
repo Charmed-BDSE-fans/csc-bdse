98a87c937b7df6b885b199852c376c56f950f17c
Lapshin Dmitry (LDVSOFT) <lapshin.dv@mail.ru>
2018-03-08 01:58:36 +0300

    Forgot profiles, sorry!

--- bdse-app/src/main/java/ru/csc/bdse/app/v1/controller/PhoneBookApiController.java
+++ bdse-app/src/main/java/ru/csc/bdse/app/v1/controller/PhoneBookApiController.java
@@ -1,11 +1,14 @@
 package ru.csc.bdse.app.v1.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.csc.bdse.app.Application;
 import ru.csc.bdse.app.common.PhoneBookApi;
 import ru.csc.bdse.app.common.PhoneBookApiControllerBase;
 import ru.csc.bdse.app.v1.phonebook.PhoneBookRecord;
 
+@Profile(Application.V1_PROFILE)
 @RestController
 @RequestMapping(PhoneBookApiController.URL_PREFIX)
 public class PhoneBookApiController extends PhoneBookApiControllerBase<PhoneBookRecord> {
--- bdse-app/src/main/java/ru/csc/bdse/app/v2/controller/PhoneBookApiController.java
+++ bdse-app/src/main/java/ru/csc/bdse/app/v2/controller/PhoneBookApiController.java
@@ -1,11 +1,14 @@
 package ru.csc.bdse.app.v2.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.csc.bdse.app.Application;
 import ru.csc.bdse.app.common.PhoneBookApi;
 import ru.csc.bdse.app.common.PhoneBookApiControllerBase;
 import ru.csc.bdse.app.v2.phonebook.PhoneBookRecord;
 
+@Profile(Application.V2_PROFILE)
 @RestController
 @RequestMapping(PhoneBookApiController.URL_PREFIX)
 public class PhoneBookApiController extends PhoneBookApiControllerBase<PhoneBookRecord> {
