b85ea187cf0256e6bda4a77cba6e6872c6ea6621
Gleb Valin <the7winds@yandex.ru>
2018-03-21 02:39:11 +0300

    fix records

--- bdse-integration-tests/src/test/java/ru/csc/bdse/app/PhoneBookNonFunctionalTest.java
+++ bdse-integration-tests/src/test/java/ru/csc/bdse/app/PhoneBookNonFunctionalTest.java
@@ -13,7 +13,6 @@ import ru.csc.bdse.kv.node.KeyValueApi;
 import ru.csc.bdse.kv.node.KeyValueApiHttpClient;
 import ru.csc.bdse.kv.node.NodeAction;
 import ru.csc.bdse.util.Containers;
-import ru.csc.bdse.util.Random;
 
 import java.util.Arrays;
 
@@ -52,6 +51,13 @@ public class PhoneBookNonFunctionalTest {
                     .around(app1)
                     .around(app2);
 
+    private final PhoneBookRecord record1 = new PhoneBookRecord("aaaa", "bbb", "123");
+    private final ru.csc.bdse.app.v2.phonebook.PhoneBookRecord record2 = new ru.csc.bdse.app.v2.phonebook.PhoneBookRecord(
+            "ccc",
+            "ddd",
+            "eee",
+            Arrays.asList("123", "456"));
+
 
     private KeyValueApi kvapi = getKvapi();
     private PhoneBookApi<PhoneBookRecord> app1api = getApp1Api();
@@ -75,19 +81,11 @@ public class PhoneBookNonFunctionalTest {
 
         kvapi.action(KVNODE_NAME, NodeAction.DOWN);
 
-        PhoneBookRecord record1 = new PhoneBookRecord(Random.randomString(), Random.randomString(), Random.randomString());
-        ru.csc.bdse.app.v2.phonebook.PhoneBookRecord record2 = new ru.csc.bdse.app.v2.phonebook.PhoneBookRecord(
-                Random.randomString(),
-                Random.randomString(),
-                Random.randomString(),
-                Arrays.asList(Random.randomString()));
-
-
         app1api.put(record1);
         app2api.put(record2);
 
-        assertTrue(app1api.get(record1.literals().stream().findFirst().get()).isEmpty());
-        assertTrue(app2api.get(record2.literals().stream().findFirst().get()).isEmpty());
+        assertTrue(app1api.get(record1.getSurname().charAt(0)).isEmpty());
+        assertTrue(app2api.get(record2.getNickname().charAt(0)).isEmpty());
 
         app1api.delete(record1);
         app1api.delete(record2);
@@ -96,14 +94,6 @@ public class PhoneBookNonFunctionalTest {
     @Test
     public void dataWasSavedIfAppRestarts() {
         // test data was saved after app restarts
-
-        PhoneBookRecord record1 = new PhoneBookRecord(Random.randomString(), Random.randomString(), Random.randomString());
-        ru.csc.bdse.app.v2.phonebook.PhoneBookRecord record2 = new ru.csc.bdse.app.v2.phonebook.PhoneBookRecord(
-                Random.randomString(),
-                Random.randomString(),
-                Random.randomString(),
-                Arrays.asList(Random.randomString()));
-
 
         app1api.put(record1);
         app2api.put(record2);
@@ -125,20 +115,13 @@ public class PhoneBookNonFunctionalTest {
         app1api = getApp1Api();
         app2api = getApp2Api();
 
-        assertEquals(record1, app1api.get(record1.literals().stream().findFirst().get()).stream().findFirst().get());
-        assertEquals(record2, app2api.get(record2.literals().stream().findFirst().get()).stream().findFirst().get());
+        assertEquals(record1, app1api.get(record1.getSurname().charAt(0)).stream().findFirst().get());
+        assertEquals(record2, app2api.get(record2.getNickname().charAt(0)).stream().findFirst().get());
     }
 
     @Test
     public void dataWasSavedIfKvNodeRestarts() {
         // test data was saved after kv-node restarts
-
-        PhoneBookRecord record1 = new PhoneBookRecord(Random.randomString(), Random.randomString(), Random.randomString());
-        ru.csc.bdse.app.v2.phonebook.PhoneBookRecord record2 = new ru.csc.bdse.app.v2.phonebook.PhoneBookRecord(
-                Random.randomString(),
-                Random.randomString(),
-                Random.randomString(),
-                Arrays.asList(Random.randomString()));
 
         app1api.put(record1);
         app2api.put(record2);
@@ -146,7 +129,7 @@ public class PhoneBookNonFunctionalTest {
         kvapi.action(KVNODE_NAME, NodeAction.DOWN);
         kvapi.action(KVNODE_NAME, NodeAction.UP);
 
-        assertEquals(record1, app1api.get(record1.literals().stream().findFirst().get()).stream().findFirst().get());
-        assertEquals(record2, app2api.get(record2.literals().stream().findFirst().get()).stream().findFirst().get());
+        assertEquals(record1, app1api.get(record1.getSurname().charAt(0)).stream().findFirst().get());
+        assertEquals(record2, app2api.get(record2.getNickname().charAt(0)).stream().findFirst().get());
     }
 }
