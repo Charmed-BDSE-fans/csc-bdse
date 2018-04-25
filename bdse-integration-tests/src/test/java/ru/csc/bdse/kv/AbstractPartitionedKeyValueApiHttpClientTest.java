2617c33f2ccacca18907dff890082ee9830cd328
Liza Tretyakova <elizabet.tretyakova@gmail.com>
2018-04-25 18:03:26 +0300

    General implementation

--- bdse-integration-tests/src/test/java/ru/csc/bdse/kv/AbstractPartitionedKeyValueApiHttpClientTest.java
+++ bdse-integration-tests/src/test/java/ru/csc/bdse/kv/AbstractPartitionedKeyValueApiHttpClientTest.java
@@ -1,10 +1,15 @@
 package ru.csc.bdse.kv;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Test;
 import ru.csc.bdse.kv.node.KeyValueApi;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Test have to be implemented
@@ -15,7 +20,9 @@ public abstract class AbstractPartitionedKeyValueApiHttpClientTest {
 
     protected abstract KeyValueApi newCluster1();
     protected abstract KeyValueApi newCluster2();
-    // Stream.generate(() -> RandomStringUtils.randomAlphanumeric(10)).limit(1000).collect(Collectors.toSet());
+
+    protected Set<String> randKeys =
+            Stream.generate(() -> RandomStringUtils.randomAlphanumeric(10)).limit(1000).collect(Collectors.toSet());
 
     protected Set<String> keys() {
         if(cluster1 == null && cluster2 == null) {
@@ -32,27 +39,44 @@ public abstract class AbstractPartitionedKeyValueApiHttpClientTest {
 
     private KeyValueApi cluster1 = newCluster1();
     private KeyValueApi cluster2 = newCluster2();
-
     private Set<String> keys = keys();
 
     @Test
     public void put1000KeysAndReadItCorrectlyOnCluster1() {
-        // TODO put 1000 keys to storage ant read it
+        for(String key: randKeys) {
+            cluster1.put(key, key.getBytes());
+        }
+        for(String key: randKeys) {
+            assertTrue(cluster1.get(key).isPresent());
+        }
     }
 
     @Test
     public void readKeysFromCluster2AndCheckLossProportion() {
-        // TODO read all keys from cluster2 and made some statistics (related to expectedKeysLossProportion)
+        double succ = 0.0;
+        double total = 0.0;
+        for(String key: randKeys) {
+            if(cluster2.get(key).isPresent()) {
+                succ += 1;
+            }
+            total += 1;
+        }
+        assertTrue((succ / total) <= expectedKeysLossProportion());
     }
 
     @Test
     public void deleteAllKeysFromCluster2() {
         // TODO try to delete all keys on cluster2
+        for(String key: randKeys) {
+            cluster2.delete(key);
+        }
     }
 
     @Test
     public void readKeysFromCluster1AfterDeletionAtCluster2() {
         // TODO read all keys from cluster1, made some statistics (related to expectedUndeletedKeysProportion)
+        Set<String> keys = cluster1.getKeys("");
+        assertTrue((float) keys.size() / randKeys.size() <= expectedUndeletedKeysProportion());
     }
 }
 
